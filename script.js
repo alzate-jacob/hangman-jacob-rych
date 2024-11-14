59f03e17c2773e2d23d3c5aedfce40b818d46536
Richard <richardzou25@gmail.com>
2024-11-14 11:10:55 -0500

    game class with base methods

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 import { wordList } from "./wordlist";
-function getRandomWord(list) {
-    return list[Math.floor(Math.random() * wordList.length)];
+// function getRandomWord(list) {
+//     return list[Math.floor(Math.random() * wordList.length)];
+// }
+class Game {
+    constructor(wordList) {
+        this.wordList = wordList;
+        this.currentWord = {};
+    }
+    start() {
+
+    }
+    getRandomWord() {
+
+    }
+    guessLetter() {
+
+    }
+    showHint() {
+
+    }
+    checkWinCon() {
+
+    }
+    checkLoseCon() {
+
+    }
+    revealWord() {
+
+    }
+    updateHangman() {
+
+    }
 }
--- wordlist.js
+++ wordlist.js
@@ -261,3 +261,4 @@ export const wordList = [
     },
 ];
 
+// modules.export = wordList;
